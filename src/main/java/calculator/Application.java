2c214093139f6e7115369e68d05a65ae94d1fcb5
cyscys06 <cyscys06@gmail.com>
2025-10-19 16:26:19 +0900

    feat(main): main 메서드에 프로그램 실행 코드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import calculator.Controller.StringController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringController calculator =  new StringController();
+        try {
+            calculator.StartProcess();
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
     }
 }
