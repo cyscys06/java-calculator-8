8b6eeed799c545c57e792ffbe1019e9305177a55
cyscys06 <cyscys06@gmail.com>
2025-10-19 16:02:06 +0900

    feat(check): StringCheck_ContainCustomSeparator 메서드 추가

---
+++ src/main/java/calculator/Controller/StringController.java
@@ -0,0 +1,14 @@
+package calculator.Controller;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class StringController {
+    public static String Input() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public static boolean StringCheck_ContainCustomSeparator(String str) {
+        return str.startsWith("//") &&  str.contains("\\n");
+    }
+}
